c44c1904d17da7a0e1d0ce4931f1ae246982f86f
gynt <gynt@users.noreply.github.com>
2021-08-10 16:19:43 +0200

    aivloader: simplified and fixed code

--- content/ucp/modules/aivloader-0.0.1/init.lua
+++ content/ucp/modules/aivloader-0.0.1/init.lua
@@ -2,6 +2,9 @@
 -- Load a subset of aivs on startup, pointing to custom files
 
 local startOfAIVMapNames = core.scanForAOB(utils.bytesToAOBString(table.pack(string.byte("aiv\\rat1.aiv", 1, -1))))
+local FILENAME_LENGTH = 50
+local CLEAR_BYTES = {}
+for i=1,FILENAME_LENGTH do table.insert(CLEAR_BYTES, 0) end
 
 local aliases = {
     rat = 0,
@@ -22,101 +25,65 @@ local aliases = {
     abbot = 15
 }
 
-local AIVFileNumberAPI = {
-    new = function(self, address)
-        local o = {
-            address = address
-        }
-        return setmetatable(o, self)
-    end,
-
-    __index = function(self, k)
-        if tonumber(k) == nil then error("not a number: " .. k) end
-        k = tonumber(k)
-        if k < 1 or k > 8 then
-            return error("key is out of bounds [1-8]")
-        end
-        return core.readString(self.address + (50*(k-1)))
-    end,
-
-    __newindex = function(self, k, v)
-        if tonumber(k) == nil then error("not a number: " .. k) end
-        k = tonumber(k)
-        if k < 1 or k > 8 then
-            return error("key is out of bounds [1-8]")
-        end
-        return core.writeString(self.address + (50*(k-1)), v)
-    end
-}
-
-local AIVFilenameAPI = {
-    new = function(self)
-        local _nested_apis = {}
-        for k=0,15 do
-            _nested_apis[k] = AIVFileNumberAPI:new(startOfAIVMapNames + (50*8*k))
-        end
-        local o = {
-            _nested_apis = _nested_apis
-        }
-        return setmetatable(o, self)
-    end,
-
-    __index = function(self, k)
-        if type(k) == "string" then
-            if tonumber(k) ~= nil then
-                k = tonumber(k)
-            else
-                if aliases[k] == nil then
-                    error("unknown alias: " .. k)
-                end
-                k = aliases[k]
+local function getFileNameAddress(ai, castle)
+    if type(ai) == "string" then
+        if tonumber(ai) ~= nil then
+            ai = tonumber(ai)
+        else
+            if aliases[ai] == nil then
+                error("unknown alias: " .. k)
             end
+            ai = aliases[ai]
         end
-        if type(k) ~= "number" then error("invalid argument 1, not a number") end
-        return self._nested_apis[k]
-    end,
-
-    __newindex = function(self, k, v)
-        if type(k) == "string" then
-            if tonumber(k) ~= nil then
-                k = tonumber(k)
-            else
-                if aliases[k] == nil then
-                    error("unknown alias: " .. k)
-                end
-                k = aliases[k]
-            end
-        end
-        if type(k) ~= "number" then error("invalid argument 1, not a number") end
-        if type(v) ~= "string" then error("invalid argument 2, not a string") end
-        if v:len() > 49 then
-            error("string is too long (max: 49): " .. v)
-        end
-        self._nested_apis[k] = v
     end
+    if type(ai) ~= "number" then error("invalid argument 1, not a number: " .. ai) end
+    if tonumber(castle) == nil then error("invalid argument 2, not a number: " .. castle) end
+    castle = tonumber(castle)
+    if castle < 1 or castle > 8 then
+        return error("invalid argument 2: key is out of bounds [1-8]")
+    end
+    return startOfAIVMapNames + (FILENAME_LENGTH*ai) + ((castle-1)*FILENAME_LENGTH)
+end
 
-}
+local function clearEntry(address)
+    core.writeBytes(address, CLEAR_BYTES)
+end
 
-local api = AIVFilenameAPI:new()
+local function writeFileName(ai, castle, fileName)
+    local address = getFileNameAddress(ai, castle)
+    if type(fileName) ~= "string" then error("invalid argument 3, not a string: " .. tostring(fileName)) end
+       if fileName:len() > 49 then
+            error("string is too long (max: 49): " .. fileName)
+        end
+    clearEntry(address)
+    core.writeString(address, fileName)
+    print("overwritten AI '" .. ai .. "' castle #" .. castle .. " at " .. address .. "d with: " .. core.readString(address))
+end
 
 return {
     enable = function(self, config)
         if config == nil then return end
-        for k, v in pairs(config) do
-            for k2, v2 in pairs(v) do
-                api[k][k2] = v2
+
+        for ai, v in pairs(config) do
+            for castle, fileName in pairs(v) do
+                writeFileName(ai, castle, fileName)
             end
         end
+
+        core.detourCode(function(registers)
+            return registers
+        end, 0x004ecb39, 7)
     end,
     disable = function(self, config)
 
     end,
 
     -- ReadOnlyTable is preventing this from being prettier (we cannot expose 'api' because it becomes frozen)
-    setAIVFileForAI = function(self, ai, castle, filename)
-        api[ai][castle] = filename
+    setAIVFileForAI = function(ai, castle, fileName)
+        writeFileName(ai, castle, fileName)
     end,
-    getAIVFileForAI = function(self, ai, castle)
-        return api[ai][castle]
-    end
+    getAIVFileForAI = function(ai, castle)
+        return core.readString(getFileNameAddress(ai, castle))
+    end,
+    getFileNameAddress = getFileNameAddress
 }
