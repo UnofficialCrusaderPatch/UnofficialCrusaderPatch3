817b63c4790468a4b75fefe7cb26cfeb56281b39
unknown <Corax34@users.noreply.github.com>
2021-08-27 09:35:29 +0200

    Improved comment

--- content/ucp/main.lua
+++ content/ucp/main.lua
@@ -41,7 +41,6 @@ package.path = BASEDIR .. "/code/?.lua"
 package.path = package.path .. ";" .. BASEDIR .. "/code/?/init.lua"
 ---Load essential ucp lua code
 core = require('core')
-hooks = require('hooks')
 utils = require('utils')
 data = {}
 data.common = require('data.common')
@@ -51,6 +50,7 @@ yaml = require('ext.yaml.yaml')
 json = require('ext.json.json')
 extensions = require('extensions')
 sha = require("ext.pure_lua_SHA.sha2")
+hooks = require('hooks')
 
 
 
@@ -96,7 +96,7 @@ if config.active == false then
     return nil
 end
 
----Overwrite gamemenu version and store game version
+---Overwrite game menu version and store game version details
 data.version.initialize(config)
 
 
