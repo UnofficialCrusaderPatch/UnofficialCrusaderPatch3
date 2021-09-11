323910991d648e857431ea877bdc30599aa5e24e
gynt <gynt@users.noreply.github.com>
2021-09-11 23:56:37 +0200

    rc file setup

--- dll/InternalData.cpp
+++ dll/InternalData.cpp
@@ -6,9 +6,63 @@
  * \date   September 2021
  *********************************************************************/
 
+#include <windows.h>
 #include "InternalData.h"
+#include "internal-data-def.h"
 
 namespace LuaIO {
+
+	struct ZipFileEntry {
+		char fileName[1020];
+		unsigned long pointer;
+	};
+
+	struct ZipFile {
+		int entryCount;
+		ZipFileEntry* entries;
+		unsigned char* data;
+	};
+
+	//https://stackoverflow.com/questions/2933295/embed-text-file-in-a-resource-in-a-native-windows-application
+	void LoadFileInResource(int name, int type, DWORD& size, const char*& data)
+	{
+		HMODULE handle = ::GetModuleHandle(NULL);
+		HRSRC rc = ::FindResource(handle, MAKEINTRESOURCE(name),
+			MAKEINTRESOURCE(type));
+
+		if (rc == 0) {
+			size = 0;
+			data = 0;
+			return;
+		}
+		else {
+			HGLOBAL rcData = ::LoadResource(handle, rc);
+			size = ::SizeofResource(handle, rc);
+			data = static_cast<const char*>(::LockResource(rcData));
+		}
+
+	}
+
+
+	void readFileEntries() {
+		ZipFile file;
+
+		
+		DWORD size = 0;
+		const char* data = NULL;
+		LoadFileInResource(IDR_INTERNALDATAFILE, LUAIDF, size, data);
+		/* Access bytes in data - here's a simple example involving text output*/
+		// The text stored in the resource might not be NULL terminated.
+		char* buffer = new char[size + 1];
+		::memcpy(buffer, data, size);
+		buffer[size] = 0; // NULL terminator
+		::printf("Contents of text file: %s\n", buffer); // Print as ASCII text
+		delete[] buffer;
+		return 0;
+	};
+
+	
+
 	const std::map<std::string, std::string> internalData = {
 		//Literal data will go here.
 		{"ucp/code/pre.lua", 
