237358bce8eabecef96114d361eecddba278474f
LeSpec <62205974+LeSpec@users.noreply.github.com>
2021-11-28 23:16:44 +0100

    run-in-background and gamespeed
    run-in-background:
    Game will keep running when alt tabbed.
    
    gamespeed:
    - replaces existing gamespeed caps by a new one
    that keeps a steady fps rate while allowing higher
    speeds
    - increased accuracy by x1000

---
+++ content/ucp/modules/gamespeed-0.0.1/init.lua
@@ -0,0 +1,76 @@
+-- imports
+local AssemblyLambda = core.AssemblyLambda
+local insertCode = core.insertCode
+local writeCode = core.writeCode
+
+-- AOBs
+local DAT_MillisecLoopMainFunc = 0x165271C -- 0x165271C
+local DAT_TickRate = 0x1FE7DC0 -- 0x1FE7DC0
+local DAT_MillisecCarry = 0xDF4228 -- 0xDF4228
+
+local speedCapEntry = 0x00487B88 -- 00487B88
+local speedCapExit = 0x00487C15 -- 00487C15
+local accuracyFix = 0x00487B53 -- 0x00487B53
+
+
+local insertAssembly = function(address, patchSize, script, valueMapping, returnTo, original)
+    local assemblyLambda = AssemblyLambda(script, valueMapping)
+    insertCode(address, patchSize, {assemblyLambda}, returnTo, original)
+end
+
+-- exports
+local exports = {}
+
+exports.enable = function(self,config)
+
+    -- enable increased accuracy
+    local script = [[
+        imul edx,0x3E8
+    ]]
+    insertAssembly(accuracyFix, 6, script, {}, nil, "before")
+
+    -- enable dynamic game speed cap
+    local script = [[
+        mov eax,cap
+        mul dword [ticksLastLoop]
+        cdq
+        div dword [timeLastLoop]
+        mov edx,eax
+        imul edx,ecx
+        sub edx,ecx
+        cmp edi,edx
+
+        jle calculateNumberOfGameTicks
+
+        pop edi
+        pop esi
+        mov dword [carry],0
+        add eax,1
+        pop ebp
+        pop ebx
+        retn 4
+    ]]
+    
+    local varDict = {
+        cap                         = config.capMillisecForOneFrame,
+        ticksLastLoop               = DAT_TickRate,
+        timeLastLoop                = DAT_MillisecLoopMainFunc,
+        calculateNumberOfGameTicks  = speedCapExit,
+        carry                       = DAT_MillisecCarry,
+    }
+    insertAssembly(speedCapEntry, 0, script, varDict) -- does not return
+
+end
+
+exports.disable = function()
+    -- disable increased accuracy
+    local oriCode = {0x8B, 0x15, 0x1C, 0x27, 0x65, 0x01}
+    writeCode(accuracyFix, oriCode)
+
+    -- disable dynamic game speed cap
+    local oriCode = {0x8B, 0x1D, 0xC8, 0x7D, 0xFE, 0x01}
+    writeCode(speedCapEntry, oriCode)
+
+end
+
+return exports
