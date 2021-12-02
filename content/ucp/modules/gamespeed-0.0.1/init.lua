887c936c56a1260f301dc7a591c6709aaae5aacc
LeSpec <62205974+LeSpec@users.noreply.github.com>
2021-12-02 16:57:18 +0100

    Update init.lua

--- content/ucp/modules/gamespeed-0.0.1/init.lua
+++ content/ucp/modules/gamespeed-0.0.1/init.lua
@@ -8,9 +8,11 @@ local DAT_MillisecLoopMainFunc = 0x165271C -- 0x165271C
 local DAT_TickRate = 0x1FE7DC0 -- 0x1FE7DC0
 local DAT_MillisecCarry = 0xDF4228 -- 0xDF4228
 
-local speedCapEntry = 0x00487B88 -- 00487B88
-local speedCapExit = 0x00487C15 -- 00487C15
-local accuracyFix = 0x00487B53 -- 0x00487B53
+local accuracyValue1 = 0x00487B43 -- 0x00487B43
+local accuracyValue2 = accuracyValue1 - 0xF -- 0x00487B34
+local accuracyFix = accuracyValue1 + 0x10 -- 0x00487B53
+local speedCapEntry = accuracyValue1 + 0x45 -- 00487B88
+local speedCapExit = accuracyValue1 + 0xD2 -- 00487C15
 
 
 local insertAssembly = function(address, patchSize, script, valueMapping, returnTo, original)
@@ -24,6 +26,9 @@ local exports = {}
 exports.enable = function(self,config)
 
     -- enable increased accuracy
+    writeCode(accuracyValue1,{0x40, 0x42, 0x0F, 0x00}) -- F4240h = 3E8h * 3E8h
+    writeCode(accuracyValue2,{0xC0, 0xBD, 0xF0, 0xFF}) -- FFF0BDC0
+
     local script = [[
         imul edx,0x3E8
     ]]
@@ -66,6 +71,8 @@ end
 
 exports.disable = function()
     -- disable increased accuracy
+    writeCode(accuracyValue1,{0xE8, 0x03, 0x00, 0x00}) -- 0x3E8
+    writeCode(accuracyValue2,{0x18, 0xFC, 0xFF, 0xFF}) -- 0xFFFFFC18
     local oriCode = {0x8B, 0x15, 0x1C, 0x27, 0x65, 0x01}
     writeCode(accuracyFix, oriCode)
 
