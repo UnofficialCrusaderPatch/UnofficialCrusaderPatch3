e4b51bfc8e30ca0ec0fc2ee06aca8f44dc39cca2
gynt <gynt@users.noreply.github.com>
2023-05-28 21:48:11 +0200

    revamped powershell build scripts

---
+++ scripts/import-yaml.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = "Stop"
+
+$ep = Get-ExecutionPolicy -Scope CurrentUser
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# Install yaml library
+if(!(Get-Module -ListAvailable -Name powershell-yaml)) {
+  Install-Module powershell-yaml -Scope CurrentUser -Force  
+}
+
+Import-Module powershell-yaml
+
+Set-ExecutionPolicy "$ep" -Scope CurrentUser
---
+++ scripts/package-module.ps1
@@ -0,0 +1,69 @@
+﻿   
+param (
+    [Parameter(Mandatory=$true)][string]$Path,
+	[Parameter(Mandatory=$true)][string]$Destination,
+    [Parameter(Mandatory=$true)][string]$BUILD_CONFIGURATION
+)
+
+$ErrorActionPreference = "Stop"
+
+# Creates a System.IO.FileSystemInfo
+$module = Get-Item -Path $Path
+
+Write-Output "Packaging module: $($module.Name)"
+
+$SIMPLE_CONFIG_MAPPING = @{
+    "DebugSecure" = "Debug";
+    "ReleaseSecure" = "Release";
+    "Debug" = "Debug";
+    "Release" = "Release";
+}
+
+$simpleBuildConfiguration=$SIMPLE_CONFIG_MAPPING[$BUILD_CONFIGURATION]
+
+# Create the module directory in the ucp-package\ucp\modules folder
+  New-Item -Path "$Destination" -Name $module.Name -ItemType "directory"
+  $moduleDir = "$Destination" + $module.Name + "\"
+
+  # If the module specifies a custom list of files to include, read that
+  $hasFilesList = Test-Path -Path ($module.FullName + "\files.xml")
+
+  # Copy the specified files, or *.lua and definition.yml if no module.files file was found.
+  if($hasFilesList) {
+    $defaultFilesNode = Select-Xml -XPath "/module/files[not(@build)]" -Path ($module.FullName + "\files.xml")
+    $buildFilesNode = Select-Xml -XPath "/module/files[@build='$simpleBuildConfiguration']" -Path ($module.FullName + "\files.xml")
+    
+    $node = $null
+    if($buildFilesNode) {
+      $node = $buildFilesNode
+    } elseif ($defaultFilesNode) {
+      $node = $defaultFilesNode
+    } else {
+      Throw "Invalid files.xml in module: " + $moduleDir
+    }
+    
+    $moduleFiles = $node.Node.file
+    foreach($moduleFile in $moduleFiles) {
+      # To allow specification of $(Configuration) in "src" in module.files, we substitute it with the right value here
+      $srcFile = $moduleFile.src.Replace("`$(Configuration)", "$simpleBuildConfiguration")
+      
+      $t = $null
+      if($moduleFile.target -eq $null) {
+        $t = "."
+      } else {
+        $t = $moduleFile.target
+      }
+      
+      # create dest if not exist
+      $destinationFolder = $moduleDir + "\" + $t + "\"
+      if (!(Test-Path -path $destinationFolder)) {
+        New-Item $destinationFolder -Type Directory
+      }
+      
+      # Copy the file, can include wildcards
+      Copy-Item ($module.FullName + "\" + $srcFile) -Destination $destinationFolder -Recurse
+    }
+  } else {
+    # TODO: avoid this wildcard. Include .lua .yml and ... only?
+    Copy-Item ($module.FullName + "\*") -Destination $moduleDir -Recurse
+  }
