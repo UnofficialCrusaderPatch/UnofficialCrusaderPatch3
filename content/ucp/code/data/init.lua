32c649b3208a95d6c0d82a4f8b0b19b82b2ba578
gynt <gynt@users.noreply.github.com>
2023-08-06 14:48:12 +0200

    addresses #49; implemented lua yaml parser; todo: serializer;

--- content/ucp/code/data/init.lua
+++ content/ucp/code/data/init.lua
@@ -4,6 +4,5 @@ data.common = require('data.common')
 data.structures = require('data.structures')
 data.version = require('data.version')
 data.cache = require('data.cache')
-data.yaml = require('data.yaml')
 
 return data
