d16b47b520f6b81a76cb043b16dd456027a0bd7c
gynt <gynt@users.noreply.github.com>
2024-03-06 13:47:17 +0100

    maploader: remove files

--- content/ucp/modules/maploader-0.1.0/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-    enable = function(self, config)
-        --The idea is to have a sub selection of maps on startup. It will make the game run faster. Perhaps point the game to a custom folder to load maps?
-        for k, v in pairs(config) do
-          modules.files:setIterationOption(k, v)
-        end
-    end,
-    disable = function(self)  end
-}
